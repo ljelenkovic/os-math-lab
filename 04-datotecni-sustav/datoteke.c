4edb82898e31d78955d3f54a47ad4e50c2150596
Leonardo Jelenkovic <leonardo.jelenkovic@fer.hr>
2022-05-17 14:22:30 +0200

    jos ideja

--- 04-datotecni-sustav/datoteke.c
+++ 04-datotecni-sustav/datoteke.c
@@ -3,11 +3,21 @@
 #include "datoteke.h"
 #include "disk.h"
 
+//neka u simulaciji (radi jednostavnosti) datotečna tablica bude u memoriji
 static struct opisnik datotecna_tablica[MAX_DATOTEKA];
+
+//slobodni blokovi na disku
+static char slobodni_blokovi[BLOCKS];
 
 void inicijaliziraj_prazni_disk()
 {
+	int i, j;
+
 	memset(datotecna_tablica, 0, sizeof(struct opisnik) * MAX_DATOTEKA);
+	for (i = 0; i < MAX_DATOTEKA; i++)
+		for (j = 0; j < BLOCKS; j++)
+			datotecna_tablica[i].blokovi[j] = -1;
+	memset(slobodni_blokovi, 0, BLOCKS);
 }
 int otvori_datoteku(char *ime, int nacin)
 {
@@ -44,7 +54,7 @@ int otvori_datoteku(char *ime, int nacin)
 			datotecna_tablica[i].nacin_rada = nacin;
 			//ne treba inicijalizirati "blokove"
 			return i;
-			}
+		}
 		else {
 			fprintf(stderr, "Nema mjesta za novu datoteku\n");
 			return -1;
@@ -61,13 +71,45 @@ int zatvori_datoteku(int id)
 
 int procitaj(int id, void *p, size_t koliko)
 {
-	//od kazalje do kazaljke+koliko, povecaj kazaljku na kraju
-	//u kojem bloku je (pocetak), u kojim jos blokovima, ...
+	//char buffer[BLOCK_SIZE];
+
+	if (id < 0 || id >= MAX_DATOTEKA ||
+		datotecna_tablica[id].ime[0] == 0 ||
+		datotecna_tablica[id].kazaljka == -1)
+	{
+		fprintf(stderr, "Krivi id\n");
+		return -1;
+	}
+	//blok_datoteke = datotecna_tablica[id].kazaljka / BLOCK_SIZE
+	//blok_diska = datotecna_tablica[id].blokovi[blok_datoteke]
+	//ako blok ne postoji, prekinuti s čitanjem i vratiti do sada procitano
+	//procitaj blok diska u privremeni buffer s dohvati_blok()
+	//kopiraj u p od datotecna_tablica[id].kazaljka do koliko ili do
+	//kraja bloka ili do datotecna_tablica[id].velicina, što prije dođe
+	//ako treba jos, dohvati iduci blok, itd.
+
 	return 0;
 }
 int zapisi(int id, void *p, size_t koliko)
 {
-	//od kazalje do kazaljke+koliko, povecaj kazaljku na kraju
-	//u kojem bloku je (pocetak), u kojim jos blokovima, ...
+	//char buffer[BLOCK_SIZE];
+
+	if (id < 0 || id >= MAX_DATOTEKA ||
+		datotecna_tablica[id].ime[0] == 0 ||
+		datotecna_tablica[id].kazaljka == -1)
+	{
+		fprintf(stderr, "Krivi id\n");
+		return -1;
+	}
+	//blok_datoteke = datotecna_tablica[id].kazaljka / BLOCK_SIZE
+	//blok_diska = datotecna_tablica[id].blokovi[blok_datoteke]
+	//ako blok ne postoji (-1), potražiti slobodni blok i dati ga datoteci;
+	//ako nema više slobodnih blokova stati s pisanjem
+	//procitaj blok diska u privremeni buffer s dohvati_blok()
+	//kopiraj iz p u buffer+datotecna_tablica[id].kazaljka 'koliko' bajtova
+	//ili kraja bloka
+	//pohrani blok na disk, povećaj kazaljku
+	//ako treba jos, dohvati/dodaj iduci blok, itd.
+
 	return 0;
 }
